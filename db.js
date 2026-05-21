8d96ec91710f4ebffeec7e18518b33ee375f5bae
Samiya Fazal <samiyacs2021@gmail.com>
2026-05-21 13:02:24 +0500

    fixed mysql backend

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
 
-module.exports = pool;
+// module.exports = pool;
--- server.js
+++ server.js
@@ -1,3 +1,80 @@
+// require("dotenv").config();
+
+// const express = require("express");
+// const cors = require("cors");
+// const mysql = require("mysql2");
+
+// const app = express();
+
+// // MIDDLEWARE
+
+// app.use(cors());
+
+// app.use(express.json());
+
+// // MYSQL CONNECTION
+
+
+// const db = mysql.createConnection(process.env.DATABASE_URL);
+
+// // CONNECT DATABASE
+
+// db.connect((err) => {
+
+//   if (err) {
+
+//     console.log("❌ MySQL Connection Error");
+
+//     console.log(err);
+
+//   }
+
+//   else {
+
+//     console.log("✅ MySQL Connected");
+
+//   }
+
+// });
+
+// // HOME ROUTE
+
+// app.get("/", (req, res) => {
+
+//   res.send("Backend Running");
+
+// });
+
+// // GET BLOGS
+
+// app.get("/api/blogs", (req, res) => {
+
+//   const sql = "SELECT * FROM Blog";
+
+//   db.query(sql, (err, result) => {
+
+//     if (err) {
+
+//       return res.status(500).json(err);
+
+//     }
+
+//     res.json(result);
+
+//   });
+
+// });
+
+// // SERVER
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+
+//   console.log(`🚀 Server running on port ${PORT}`);
+
+// }); 
+
 require("dotenv").config();
 
 const express = require("express");
@@ -9,60 +86,42 @@ const app = express();
 // MIDDLEWARE
 
 app.use(cors());
-
 app.use(express.json());
 
 // MYSQL CONNECTION
-
 
 const db = mysql.createConnection(process.env.DATABASE_URL);
 
 // CONNECT DATABASE
 
 db.connect((err) => {
-
   if (err) {
-
     console.log("❌ MySQL Connection Error");
-
     console.log(err);
-
-  }
-
-  else {
-
+  } else {
     console.log("✅ MySQL Connected");
-
   }
-
 });
 
 // HOME ROUTE
 
 app.get("/", (req, res) => {
-
   res.send("Backend Running");
-
 });
 
 // GET BLOGS
 
 app.get("/api/blogs", (req, res) => {
-
   const sql = "SELECT * FROM Blog";
 
   db.query(sql, (err, result) => {
-
     if (err) {
-
+      console.log(err);
       return res.status(500).json(err);
-
     }
 
     res.json(result);
-
   });
-
 });
 
 // SERVER
@@ -70,7 +129,5 @@ app.get("/api/blogs", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-
   console.log(`🚀 Server running on port ${PORT}`);
-
-}); 
+});
