217ff1194c49160edbca250cfa8eb2751dbd05da
Samiya Fazal <samiyacs2021@gmail.com>
2026-05-21 12:42:06 +0500

    fixed mysql railway connection

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ app.use(express.json());
 // MYSQL CONNECTION
 
 
-const db = mysql.createConnection({
-  uri: process.env.DATABASE_URL
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
+
 // CONNECT DATABASE
 
 db.connect((err) => {
